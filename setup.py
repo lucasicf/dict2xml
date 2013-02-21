22ca503e71d35d21f1a3ae49e871351d1ef0bfa0
Lucas Inojosa <lucas.inojosa@gmail.com>
2013-02-21 15:03:40 -0300

    Adding support for installing with 'python setup.py install'

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='dict2xml',
+    version='0.0.1',
+    description='JSON-like dictionary to XML converter',
+    author=u'Lucas Inojosa',
+    author_email='lucas.inojosa@gmail.com',
+    url='http://github.org/lucasicf/dict2xml',
+    packages=[],
+)
